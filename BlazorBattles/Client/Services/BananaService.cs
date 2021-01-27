5432e93dde8a10fc4ea2a29f174de9dab4056c19
Adam Spice <adam@adam-spice.online>
2021-01-28 07:05:47 +1300

    Added build and unit services

---
+++ BlazorBattles/Client/Services/BananaService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BlazorBattles.Client.Services
+{
+    public class BananaService : IBananaService
+    {
+        public int Bananas { get; set; } = 1000;
+
+        public event Action OnChnage;
+
+        public void AddBananas(int amount)
+        {
+            Bananas += amount;
+            BananasChnaged();
+        }
+
+        public void EatBananas(int amount)
+        {
+            Bananas -= amount;
+            BananasChnaged();
+        }
+
+        void BananasChnaged() => OnChnage.Invoke();
+    }
+}
---
+++ BlazorBattles/Shared/Unit.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorBattles.Shared
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Attack { get; set; }
+        public int Defence { get; set; }
+        public int HitPoints { get; set; } = 100;
+        public int BananaCost { get; set; }
+
+    }
+}
