13575cc798461523c3da2aa41a9462a9ee60efb2
Adam Spice <adam@adam-spice.online>
2021-01-29 07:20:19 +1300

    Receive Units from the SQLite Database

--- BlazorBattles/Server/Controllers/UnitController.cs
+++ BlazorBattles/Server/Controllers/UnitController.cs
@@ -1,6 +1,6 @@
-﻿using BlazorBattles.Shared;
+﻿using BlazorBattles.Server.Data;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace BlazorBattles.Server.Controllers
@@ -9,17 +9,18 @@ namespace BlazorBattles.Server.Controllers
     [ApiController]
     public class UnitController : ControllerBase
     {
-        public IList<Unit> Units { get; } = new List<Unit>
+        private readonly DataContext _context;
+
+        public UnitController(DataContext context)
         {
-            new Unit{ Id = 1, Title = "Knight", Attack = 10, Defence = 10, BananaCost = 100},
-            new Unit{ Id = 2, Title = "Archer", Attack = 15, Defence = 5, BananaCost = 150 },
-            new Unit{ Id = 3, Title = "Mage", Attack = 20, Defence = 1, BananaCost = 200 }
-        };
+            _context = context;
+        }
 
         [HttpGet]
         public async Task<IActionResult> GetUnits()
         {
-            return Ok(Units);
+            var units = await _context.Units.ToListAsync();
+            return Ok(units);
         }
     }
 }
