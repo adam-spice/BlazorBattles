9331ddb758b215ac4b964cdd4fa13d6843345dbb
Adam Spice <adam@adam-spice.online>
2021-01-29 08:38:35 +1300

    Added User model and Auth Repository

--- BlazorBattles/Server/Startup.cs
+++ BlazorBattles/Server/Startup.cs
@@ -22,9 +22,9 @@ namespace BlazorBattles.Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
