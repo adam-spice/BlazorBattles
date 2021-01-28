dbc74a41eb85e0b88900e8c9713c545d69d545e7
Adam Spice <adam@adam-spice.online>
2021-01-29 10:00:33 +1300

    Client authentication added

--- BlazorBattles/Client/Services/IAuthService.cs
+++ BlazorBattles/Client/Services/IAuthService.cs
@@ -6,5 +6,7 @@ namespace BlazorBattles.Client.Services
     public interface IAuthService
     {
         Task<ServiceResponse<int>> Register(UserRegister request);
+        Task<ServiceResponse<string>> Login(UserLogin request);
+
     }
 }
