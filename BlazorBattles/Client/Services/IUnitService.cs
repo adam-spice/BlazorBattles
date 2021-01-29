3f420fcba4e1e99c471d8723b2382ac75aa06be8
Adam Spice <adam@adam-spice.online>
2021-01-29 14:27:45 +1300

    Implemented building user units

--- BlazorBattles/Client/Services/IUnitService.cs
+++ BlazorBattles/Client/Services/IUnitService.cs
@@ -8,7 +8,7 @@ namespace BlazorBattles.Client.Services
     {
         IList<Unit> Units { get; set; }
         IList<UserUnit> MyUnits { get; set; }
-        void AddUnit(int unitId);
+        Task AddUnit(int unitId);
         Task LoadUnitsAsync();
     }
 }
